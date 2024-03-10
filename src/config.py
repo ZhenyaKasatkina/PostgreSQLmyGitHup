840304f50eb9c1eea6d2cfb3ebb002997ed4d6f2
ZhenyaKasatkina <knopisha.zh@gmail.com>
2024-03-10 18:29:40 +0300

    final data

---
+++ src/config.py
@@ -0,0 +1,21 @@
+import os.path
+from configparser import ConfigParser
+
+FILE = 'database.ini'
+ROOT_DIR = os.path.dirname(__file__)
+
+
+def config(filename=os.path.join(ROOT_DIR, FILE), section='postgresql'):
+    # create a parser
+    parser = ConfigParser()
+    # read config file
+    parser.read(filename)
+    db = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception(
+            'Section {0} is not found in the {1} file.'.format(section, filename))
+    return db
